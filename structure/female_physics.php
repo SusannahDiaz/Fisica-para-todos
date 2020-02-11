6c28012270eaed5eaa3a0ebf38376907687948a0
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 13:53:41 -0600

    Media queries was updated

--- structure/female_physics.php
+++ structure/female_physics.php
@@ -29,7 +29,57 @@
                     <img class="img-responsive charlatitle" src="img/video_reportaje.png" alt="">
                 </div>
             </div>
-            <div class="row">
+            <!--xs and md-->
+            <div class="row visible-xs visible-sm hidden-md hidden-lg">
+                <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_brandan.png" alt="">
+			            <p class="smaller">F&iacute;sica M&eacute;dica</br>Mar&iacute;a Ester Brandan</p>
+                    </div>
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_cetto.png" alt="">
+			            <p class="smaller">F&iacute;sica Cu&aacute;ntica</br>Ana Mar&iacute;a Cetto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row visible-xs visible-sm hidden-md hidden-lg">
+                <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_volke.png" alt="">
+			            <p class="smaller">&Oacute;ptica</br>Karen Volke</p>
+                    </div>
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_jauregui.png" alt="">
+			            <p class="smaller">LANMAC</br>Roc&iacute;o J&aacute;uregui</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row visible-xs visible-sm hidden-md hidden-lg">
+                <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_mondragon.png" alt="">
+			            <p class="smaller">F&iacute;sica de part&iacute;culas</br>Myriam Mondrag&oacute;n</p>
+                    </div>
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_diaz.png" alt="">
+			            <p class="smaller">F&iacute;sica Qu&iacute;mica</br>D&iacute;az</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row visible-xs visible-sm hidden-md hidden-lg">
+                <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_solis.png" alt="">
+			            <p class="smaller">LEMA</br>Corina Sol&iacute;s</p>
+                    </div>
+                    <div class="col-xs-6 col-md-3 col-md-offset-0">
+                        <img class="img-responsive" src="img/video_noguez.png" alt="">
+			            <p class="smaller">Nanociencias</br>Cecilia Noguez</p>
+                    </div>
+                </div>
+            </div>
+            <!--md and large-->
+            <div class="row visible-md visible-lg hidden-xs hidden-sm">
                 <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
                     <div class="col-xs-6 col-md-3 col-md-offset-0">
                         <img class="img-responsive charlas" src="img/video_brandan.png" alt="">
