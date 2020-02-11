e70879b115499d2efcd9a0ec5f08ed9827544694
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 16:00:56 -0600

    new structure, videos and gallery icons was added

---
+++ structure/chats/gallery_romero.php
@@ -0,0 +1,14 @@
+<img class="img-responsive charlas" src="img/charla_divulgacion_romero.png" alt="">
+<div class="col-xs-6">
+    <!--<a href="" data-toggle="lightbox" data-gallery="youtubevideos">-->
+        <img class="img-responsive img-center small" src="img/icon_video_aqua.png" alt="">
+    <!--</a>-->
+    <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="img/nina_ciencia_aurore.jpg" data-title=""></div>
+    <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="img/icon_video_aqua.png" data-title=""></div>
+</div>
+<div class="col-xs-6">
+    <!--<a href="img/charla_divulgacion_noguez.png" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">-->
+        <img class="img-responsive img-center small" src="img/icon_galeria_aqua.png" alt="">
+    <!--</a>-->
+</div>
+<p class="small">5 de mayo</br>Victor Romero</p>			            
