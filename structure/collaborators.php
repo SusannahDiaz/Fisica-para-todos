9283ddb9c2a88e8989fd2625527d1825d3747807
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 20:09:27 -0600

    padding was fixed

--- structure/collaborators.php
+++ structure/collaborators.php
@@ -7,50 +7,50 @@
             </div>
             <div class="row">
                 <div class="col-xs-6 col-md-2 text-center">
-                    <img src="img/colaborador_aleida.png" class="img-responsive img-centered charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_aleida.png" class="img-responsive img-centered collab">
+                    <p class="collab">
                         <strong>Aleida Rueda</strong>
                         </br>Responsable T&eacute;cnica del Proyecto y Coordinadora de Comunicaci&oacute;n del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 text-center">
-                    <img src="img/colaborador_delia.png" class="img-responsive img-centered charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_delia.png" class="img-responsive img-centered collab">
+                    <p class="collab">
                         <strong>Delia O'Reilly</strong>
                         </br>Responsable Administrativa del Proyecto y Secretaria Administraativa del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 visible-xs visible-sm text-center">
-                    <img src="img/colaborador_rosario.png" class="img-responsive img-centered  charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_rosario.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
                         <strong>Rosario Paredes</strong>
                         </br>Coordinadora Docente del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 visible-md visible-lg hidden-xs hidden-sm text-center">
-                    <img src="img/colaborador_rosario.png" class="img-responsive img-centered  charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_rosario.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
                         <strong>Rosario Paredes</strong>
                         </br>Coordinadora Docente</br> del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
                         <strong>Carina Cosme</strong>
                         </br>Asistente en la Coordinaci&oacute;n del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 text-center">
-                    <img src="img/colaborador_luis.png" class="img-responsive img-centered charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_luis.png" class="img-responsive img-centered collab">
+                    <p class="collab">
                         <strong>Luis Novoa</strong>
                         </br>Dise&ntilde;ador gr&aacute;fico del IF
                     </p>
                 </div>
                 <div class="col-xs-6 col-md-2 text-center">
-                    <img src="img/colaborador_daniel.png" class="img-responsive img-centered charlas">
-                    <p class="smaller">
+                    <img src="img/colaborador_daniel.png" class="img-responsive img-centered collab">
+                    <p class="collab">
                         <strong>Daniel Rosales</strong>
                         </br>Webmaster del IF
                     </p>
@@ -59,42 +59,42 @@
             <!--Second row-->
             <!--<div class="row">
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered collab">
                     <p class="small">
                         <strong></strong>
                         
                     </p>
                 </div>
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered collab">
                     <p class="small">
                         <strong></strong>
                         
                     </p>
                 </div>
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  collab">
                     <p class="small">
                         <strong></strong>
                         
                     </p>
                 </div>
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered  collab">
                     <p class="small">
                         <strong></strong>
                         
                     </p>
                 </div>
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered collab">
                     <p class="small">
                         <strong></strong>
                         
                     </p>
                 </div>
                 <div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0 text-center">
-                    <img src="img/colaborador_carina.png" class="img-responsive img-centered charlas">
+                    <img src="img/colaborador_carina.png" class="img-responsive img-centered collab">
                     <p class="small">
                         <strong></strong>
                         
