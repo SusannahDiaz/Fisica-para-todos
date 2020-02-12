98499163a4c993bd5fdb93d7c30a83a929772c9b
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 18:16:42 -0600

    Data was updated

--- structure/chats/gallery_barrera.php
+++ structure/chats/gallery_barrera.php
@@ -3,8 +3,6 @@
     <!--<a href="" data-toggle="lightbox" data-gallery="youtubevideos">-->
         <img class="img-responsive img-center small" src="img/icon_video_aqua.png" alt="">
     <!--</a>-->
-    <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="img/nina_ciencia_aurore.jpg" data-title=""></div>
-    <div data-toggle="lightbox" data-gallery="hidden-images" data-remote="img/icon_video_aqua.png" data-title=""></div>
 </div>
 <div class="col-xs-6">
     <!--<a href="img/charla_divulgacion_noguez.png" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">-->
