ee7cf86fb6c65d592e2c3d602e28045dd52a984b
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 19:18:40 -0600

    Opacity was added to icons

--- structure/chats/gallery_barrera.php
+++ structure/chats/gallery_barrera.php
@@ -1,12 +1,12 @@
 <img class="img-responsive charlas" src="img/charla_divulgacion_barrera.png" alt="">
 <div class="col-xs-6">
     <!--<a href="" data-toggle="lightbox" data-gallery="youtubevideos">-->
-        <img class="img-responsive img-center small" src="img/icon_video_aqua.png" alt="">
+        <img class="img-responsive img-center small opacity" src="img/icon_video_aqua.png" alt="">
     <!--</a>-->
 </div>
 <div class="col-xs-6">
     <!--<a href="img/charla_divulgacion_noguez.png" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">-->
-        <img class="img-responsive img-center small" src="img/icon_galeria_aqua.png" alt="">
+        <img class="img-responsive img-center small opacity" src="img/icon_galeria_aqua.png" alt="">
     <!--</a>-->
 </div>
 <p class="small">19 de mayo</br>Rub&eacute;n Barrera</p>		            
--- structure/chats/gallery_brandan.php
+++ structure/chats/gallery_brandan.php
@@ -1,12 +1,12 @@
 <img class="img-responsive charlas" src="img/charla_divulgacion_brandan.png" alt="">
 <div class="col-xs-6">
     <!--<a href="" data-toggle="lightbox" data-gallery="youtubevideos">-->
-        <img class="img-responsive img-center small" src="img/icon_video_aqua.png" alt="">
+        <img class="img-responsive img-center small opacity" src="img/icon_video_aqua.png" alt="">
     <!--</a>-->
 </div>
 <div class="col-xs-6">
     <!--<a href="img/charla_divulgacion_noguez.png" data-toggle="lightbox" data-gallery="hidden-images" class="col-4">-->
-        <img class="img-responsive img-center small" src="img/icon_galeria_aqua.png" alt="">
+        <img class="img-responsive img-center small opacity" src="img/icon_galeria_aqua.png" alt="">
     <!--</a>-->
 </div>
 <p class="small">11 de marzo</br>Mar&iacute;a Ester Brandan</p>			            
