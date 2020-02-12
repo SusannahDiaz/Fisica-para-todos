9c757d691d7978c3e49949588711d2ffd659af27
DannasCornell <dannas.cornell@gmail.com>
2020-02-11 19:54:10 -0600

    padding was fixed

--- structure/collaborators.php
+++ structure/collaborators.php
@@ -6,49 +6,49 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-6 col-md-2 col-md-offset-0 text-center">
+                <div class="col-xs-6 col-md-2 text-center">
                     <img src="img/colaborador_aleida.png" class="img-responsive img-centered charlas">
                     <p class="smaller">
                         <strong>Aleida Rueda</strong>
                         </br>Responsable T&eacute;cnica del Proyecto y Coordinadora de Comunicaci&oacute;n del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 text-center">
+                <div class="col-xs-6 col-md-2 text-center">
                     <img src="img/colaborador_delia.png" class="img-responsive img-centered charlas">
                     <p class="smaller">
                         <strong>Delia O'Reilly</strong>
                         </br>Responsable Administrativa del Proyecto y Secretaria Administraativa del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 visible-xs visible-sm text-center">
+                <div class="col-xs-6 col-md-2 visible-xs visible-sm text-center">
                     <img src="img/colaborador_rosario.png" class="img-responsive img-centered  charlas">
                     <p class="smaller">
                         <strong>Rosario Paredes</strong>
                         </br>Coordinadora Docente del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 visible-md visible-lg hidden-xs hidden-sm text-center">
+                <div class="col-xs-6 col-md-2 visible-md visible-lg hidden-xs hidden-sm text-center">
                     <img src="img/colaborador_rosario.png" class="img-responsive img-centered  charlas">
                     <p class="smaller">
                         <strong>Rosario Paredes</strong>
                         </br>Coordinadora Docente</br> del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 text-center">
+                <div class="col-xs-6 col-md-2 text-center">
                     <img src="img/colaborador_carina.png" class="img-responsive img-centered  charlas">
                     <p class="smaller">
                         <strong>Carina Cosme</strong>
                         </br>Asistente en la Coordinaci&oacute;n del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 text-center">
+                <div class="col-xs-6 col-md-2 text-center">
                     <img src="img/colaborador_luis.png" class="img-responsive img-centered charlas">
                     <p class="smaller">
                         <strong>Luis Novoa</strong>
                         </br>Dise&ntilde;ador gr&aacute;fico del IF
                     </p>
                 </div>
-                <div class="col-xs-6 col-md-2 col-md-offset-0 text-center">
+                <div class="col-xs-6 col-md-2 text-center">
                     <img src="img/colaborador_daniel.png" class="img-responsive img-centered charlas">
                     <p class="smaller">
                         <strong>Daniel Rosales</strong>
