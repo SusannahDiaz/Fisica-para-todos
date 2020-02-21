4000d6b40df583a8f056dd5fc5da2d7a1fd29c80
DannasCornell <dannas.cornell@gmail.com>
2020-02-21 12:40:28 -0600

    Students section was added

---
+++ structure/students.php
@@ -0,0 +1,72 @@
+<section class="proyectos moradoo" id="perfiles">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                        <h3>ESTUDIANTES ASOCIADOS, TESISTAS Y COLABORADORES EXTERNOS</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6 col-md-2 col-md-offset-1 text-center">
+                    <img src="img/collaborators/colaboradores_evelyn.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>Evelyn Carolina</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_humberto.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>Humberto Basilio</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_maya.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
+                        <strong>Mayag&uuml;el Casa&ntilde;as</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_geraldine.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
+                        <strong>Geraldine Castro</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_luciana.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
+                        <strong>Luciana de la Fuente</strong>
+                    </p>
+                </div>
+            <!--Second row-->
+                <div class="col-xs-6 col-md-2 col-md-offset-1 text-center">
+                    <img src="img/collaborators/colaboradores_arturo.png" class="img-responsive img-centered  collab">
+                    <p class="collab">
+                        <strong>Arturo L&oacute;pez</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_yanine.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>Yanine Quiroz</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_andres.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>Andr&eacute;s Res&eaecute;ndiz</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_david.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>David Salcedo</strong>
+                    </p>
+                </div>
+                <div class="col-xs-6 col-md-2 text-center">
+                    <img src="img/collaborators/colaboradores_carlos.png" class="img-responsive img-centered collab">
+                    <p class="collab">
+                        <strong>Carlos S&aacute;nchez</strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+   </section>
